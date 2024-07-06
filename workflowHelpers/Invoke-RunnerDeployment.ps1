4952948a643450feb18ac841d9dbcb421b559943
Stanisław Horna <stanislawhorna@outlook.com>
2024-07-06 19:37:50 +0200

    add workflow for runners deployment

---
+++ workflowHelpers/Invoke-RunnerDeployment.ps1
@@ -0,0 +1,29 @@
+param(
+    [int]$WorkersNumber,
+    [string]$OrgName,
+    [string]$AccessToken,
+    [string]$Labels,
+    [string]$DockerImage
+)
+
+New-Variable -Name 'CONTAINER_COMMON_NAME' -Value "GH-runner" -Option ReadOnly -Scope Script
+
+function Invoke-Main {
+    Start-Containers
+}
+
+function Start-Containers {
+    for($i=0; $i -lt $WorkersNumber; $i++){
+        $ContainerName = "$CONTAINER_COMMON_NAME-$i"
+        $id = docker run `
+            --detach `
+            --env ORGANIZATION="$OrgName" `
+            --env ACCESS_TOKEN="$AccessToken" `
+            --env LABELS="$($Labels.Replace(' ',''))" `
+            --name "$ContainerName" `
+            $DockerImage
+        Write-Host "$ContainerName started (container id: $id)"
+    }
+}
+
+Invoke-Main
---
+++ workflowHelpers/Invoke-RunnerRemoval.ps1
@@ -0,0 +1,38 @@
+New-Variable -Name 'CONTAINER_COMMON_NAME' -Value "GH-runner" -Option ReadOnly -Scope Script
+
+function Invoke-Main {
+    Stop-Containers
+}
+
+function Stop-Containers {
+    $ContainersToRemove = $(Get-ContainerName)
+    if ($ContainersToRemove.count -le 0){
+        Write-Host "No runners to remove"
+        return
+    }
+    foreach ($cName in $ContainersToRemove){
+        try {
+            docker stop $cName
+            $NameToDisplay = docker rm -f -v $cName
+            Write-Host "$NameToDisplay removed"
+        }
+        catch {
+            continue
+        }
+    }
+}
+
+function Get-ContainerName {
+    return (
+        docker ps -a --format json `
+            | ConvertFrom-Json `
+            | ForEach-Object {
+                $_.Names
+            } `
+            | Where-Object {
+                $_ -like "$CONTAINER_COMMON_NAME*"
+            }
+    )
+}
+
+Invoke-Main
